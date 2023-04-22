06df491cfe2403912da83342233f6b536862ec43
vanthony13 <81977315+vanthony13@users.noreply.github.com>
2023-04-22 20:05:11 -0100

    Delete portfolio-master directory

--- portfolio-master/js/script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.getElementsByClassName("hamburger")[0]
-const mobileNavs = document.getElementsByClassName("nav-links")[0]
-
-hamburger.addEventListener("click", () => {
-  mobileNavs.classList.toggle("active")
-})
